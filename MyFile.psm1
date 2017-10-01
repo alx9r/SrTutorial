1d5461ff0eca638afcfd7897bdc86a206dd9a697
alx9r <eggy.island9@gmail.com>
2017-10-01 15:59:03 -0700

    mention remaining parameters

--- MyFile.psm1
+++ MyFile.psm1
@@ -9,6 +9,15 @@ class MyFile
     [string]
     $Path
 
+    $Content
+    $Archive
+    $Hidden
+    $NoScrubData
+    $NotContentIndexed
+    $ReadOnly
+    $System
+    $Temporary
+
     Set(){}
     [bool] Test(){ return $true }
     [MyFile] Get(){ return $this }
