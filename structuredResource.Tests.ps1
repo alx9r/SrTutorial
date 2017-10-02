fadb60a89b05ed4d060b4e1a56d06c0dbe777ff8
alx9r <eggy.island9@gmail.com>
2017-10-01 17:20:48 -0700

    turn on integration tests add test arguments

--- structuredResource.Tests.ps1
+++ structuredResource.Tests.ps1
@@ -1,7 +1,21 @@
 Import-Module SrTutorial -Force
 
 Describe 'MyFile' {
-    foreach ( $test in (New-StructuredResourceTest MyFile SrTutorial -Kind Unit ) )
+    foreach ( $test in (New-StructuredResourceTest MyFile SrTutorial  @{
+        Path = [System.IO.Path]::GetTempFileName()
+        Content = @"
+            some
+            multiline
+            content
+"@
+        Archive = $true
+        Hidden = $true
+        NoScrubData = $true
+        NotContentIndexed = $true
+        ReadOnly = $true
+        System = $true
+        Temporary = $true
+    }))
     {
         It $test.FullMessage {
             $test | Invoke-StructuredResourceTest
